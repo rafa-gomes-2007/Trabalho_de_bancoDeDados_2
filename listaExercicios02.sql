43e56ded39e3e9aa4ff0ca27c9996d0d7f7b75c5
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-25 21:35:45 -0300

    commit 6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -40,3 +40,50 @@ DELIMITER ;
 
 CALL sp_ContarLivrosPorCategoria('CategoriaDesejada', @total);
 SELECT @total;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoria_nome VARCHAR(100), OUT possui_livros BOOLEAN)
+BEGIN
+    SELECT EXISTS(
+        SELECT 1
+        FROM Livro
+        INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoria_nome
+    ) INTO possui_livros;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_VerificarLivrosCategoria('CategoriaDesejada', @possui_livros);
+SELECT @possui_livros;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosAteAno(IN ano_limite INT)
+BEGIN
+    SELECT Titulo
+    FROM Livro
+    WHERE Ano_Publicacao <= ano_limite;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_LivrosAteAno(AnoDesejado);
+
+DELIMITER //
+
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_TitulosPorCategoria('CategoriaDesejada');
