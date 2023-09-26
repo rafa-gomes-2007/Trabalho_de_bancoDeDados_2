b8069e5b445d950f3f35b94e58bfa5fef8009be5
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-25 21:38:24 -0300

    commit 9

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -133,3 +133,18 @@ DELIMITER ;
 
 CALL sp_AutorMaisAntigo(@nome_autor);
 SELECT @nome_autor;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS NomeAutor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_LivrosESeusAutores();
