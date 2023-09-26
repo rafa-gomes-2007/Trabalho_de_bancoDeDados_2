0d06dcffebcd5f3454bfb4c52a9985860c558686
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-25 21:33:42 -0300

    commit 3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -24,3 +24,19 @@ END;
 DELIMITER ;
 
 CALL sp_LivrosPorCategoria('CategoriaEscolhida');
+
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100), OUT total INT)
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_ContarLivrosPorCategoria('CategoriaDesejada', @total);
+SELECT @total;
