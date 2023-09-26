3765b686c526ce70c88a374d0da4273964c90d76
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-25 21:37:13 -0300

    commit 7

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -87,3 +87,31 @@ END;
 DELIMITER ;
 
 CALL sp_TitulosPorCategoria('CategoriaDesejada');
+
+DELIMITER //
+
+CREATE PROCEDURE sp_AdicionarLivro(
+    IN titulo VARCHAR(255),
+    IN editora_id INT,
+    IN ano_publicacao INT,
+    IN numero_paginas INT,
+    IN categoria_id INT,
+    OUT mensagem VARCHAR(255)
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR 1062
+    BEGIN
+        SET mensagem = 'Erro: Título duplicado. O livro já existe.';
+    END;
+
+    INSERT INTO Livro (Titulo, Editora_ID, Ano_Publicacao, Numero_Paginas, Categoria_ID)
+    VALUES (titulo, editora_id, ano_publicacao, numero_paginas, categoria_id);
+
+    SET mensagem = 'Livro adicionado com sucesso.';
+END;
+//
+
+DELIMITER ;
+
+CALL sp_AdicionarLivro('Novo Livro', 1, 2023, 250, 2, @mensagem);
+SELECT @mensagem;
