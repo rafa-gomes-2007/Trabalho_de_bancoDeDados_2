efc394e0e8f1c587ab101501e13f79c82ed4e67e
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-25 21:31:54 -0300

    commit 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,18 @@ END;
 DELIMITER ;
 
 CALL sp_ListarAutores(); 
+
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_LivrosPorCategoria('CategoriaEscolhida');
